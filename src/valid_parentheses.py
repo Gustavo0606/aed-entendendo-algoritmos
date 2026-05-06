14edfbb72c1ca1092bedd4420422e6ac4019ab66
Gustavo0606 <gustavovilelas22@gmail.com>
2026-05-06 09:22:51 -0300

    feat - implementa valid_parentheses

--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -2,4 +2,28 @@
 
 
 def is_valid_parentheses(string: str) -> bool:
-    raise NotImplementedError
+    pilhaTeste = []
+    if len(string) == 0:
+        return True
+    if string[0] == ")" or string[0] == "]" or string[0] == "}":
+        return False
+    for i in range(len(string)):
+        parentese = string[i]
+        if parentese in "([{":
+            pilhaTeste.append(parentese)
+        elif parentese in ")}]":
+            if not pilhaTeste:
+                return False
+            removido = pilhaTeste.pop()
+            if parentese == ")" and removido != "(":
+                return False
+            if parentese == "]" and removido != "[":
+                return False
+            if parentese == "}" and removido != "{":
+                return False
+        else:
+            return False
+    if len(pilhaTeste) == 0:
+        return True
+    else:
+        return False
