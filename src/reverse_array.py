4c63fe150d5622146efc512e30fd243b9ce25834
Gustavo0606 <gustavovilelas22@gmail.com>
2026-05-06 07:51:20 -0300

    style - resolve problemas de formatação

--- src/reverse_array.py
+++ src/reverse_array.py
@@ -3,9 +3,9 @@ from src.my_array import MyArray
 
 def reverse_array(array: MyArray) -> MyArray:
     esquerda = 0
-    direita = len(array) -1
+    direita = len(array)-1
     while esquerda < direita:
-        array[esquerda] , array [direita] = array [direita], array [esquerda]
+        array[esquerda], array[direita] = array[direita], array[esquerda]
         esquerda += 1
         direita -= 1
     return array
