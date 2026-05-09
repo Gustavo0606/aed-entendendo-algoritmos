7b83d38df77e9ee99db0191a080e23456ec07fc6
Gustavo0606 <gustavovilelas22@gmail.com>
2026-05-09 08:41:32 -0300

    feat - implementa remove_duplicates

--- src/remove_duplicates.py
+++ src/remove_duplicates.py
@@ -2,4 +2,13 @@ from src.my_node import MyNode
 
 
 def remove_duplicates(head: MyNode) -> MyNode:
-    raise NotImplementedError
+    cabeca = head
+    while cabeca:
+        proximo = cabeca
+        while proximo.next:
+            if cabeca.value == proximo.next.value:
+                proximo.next = proximo.next.next
+            else:
+                proximo = proximo.next
+        cabeca = cabeca.next
+    return head
