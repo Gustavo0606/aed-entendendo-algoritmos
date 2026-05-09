6a5fca69a439a9b3ca3bdc1e13555ae6a9809be1
Gustavo0606 <gustavovilelas22@gmail.com>
2026-05-08 21:26:31 -0300

    feat - implementa reverse_linked_list

--- src/reverse_linked_list.py
+++ src/reverse_linked_list.py
@@ -2,4 +2,15 @@ from src.my_node import MyNode
 
 
 def reverse_linked_list(head: MyNode) -> MyNode:
-    raise NotImplementedError
+    anterior = None
+    atual = head
+    length = 0
+    while atual is not None:
+        proximo = atual.next
+        atual.next = anterior
+        anterior = atual
+        atual = proximo
+        length += 1
+    if length == 0:
+        return None
+    return anterior
